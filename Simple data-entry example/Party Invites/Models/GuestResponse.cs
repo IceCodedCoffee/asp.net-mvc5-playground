4b42daa11a247e31d29ea206fb28f79a5be524c8
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-17 18:47:34 +0100

    Simple data-entry: Add model GuestResponse

---
+++ Simple data-entry example/Party Invites/Models/GuestResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Party_Invites.Models
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public bool? WillAttend { get; set; }
+    }
+}
