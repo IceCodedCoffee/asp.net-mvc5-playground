1cece8ababd778a85393632c929a804d2ec9c7af
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-17 18:41:40 +0100

    Simple data-entry: Add HomeController & Index-View

---
+++ Simple data-entry example/Party Invites/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Party_Invites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
