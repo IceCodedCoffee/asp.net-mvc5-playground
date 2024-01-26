b4a830797a08ffbb401dca428ebb14b48a206655
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-26 05:28:00 +0100

    Razor: Add different Layout & ViewStart combinations

---
+++ Razor/Razor/Controllers/Home1Controller.cs
@@ -0,0 +1,27 @@
+﻿using Razor.Models;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+    public class Home1Controller : Controller
+    {
+        Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult NameAndPrice()
+        {
+            return View(myProduct);
+        }
+    }
+}
---
+++ Razor/Razor/Controllers/Home3Controller.cs
@@ -0,0 +1,22 @@
+﻿using Razor.Models;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+    public class Home3Controller : Controller
+    {
+        Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+    }
+}
