cf62acb1acc9d172d745613effc9f4705408c197
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-29 01:15:12 +0100

    EssentialTools: Setup sample project

---
+++ Essential Tools/EssentialTools/Models/LinqValueCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EssentialTools.Models
+{
+    public class LinqValueCalculator
+    {
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return products.Sum(p => p.Price);
+        }
+    }
+}
---
+++ Essential Tools/EssentialTools/Models/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace EssentialTools.Models
+{
+    public class ShoppingCart
+    {
+        private LinqValueCalculator calc;
+
+        public ShoppingCart(LinqValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+
+        public IEnumerable<Product> Products { get; set; }
+
+        public decimal CalculateProductTotal()
+        {
+            return calc.ValueProducts(Products);
+        }
+    }
+}
