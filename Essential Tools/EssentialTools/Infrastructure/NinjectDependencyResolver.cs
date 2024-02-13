6af07c97fa9a8c3f27af8b48791c2ea81079e30b
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-02-13 17:04:34 +0100

    EssentialTools: DI: Bind interface to implementing class

---
+++ Essential Tools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿using EssentialTools.Models;
+using EssentialTools.Models.Interfaces;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace EssentialTools.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+        }
+    }
+}
