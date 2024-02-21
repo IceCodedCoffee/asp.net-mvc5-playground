19d872a6547020bfbe879096c31846fb40a63fef
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-02-21 02:00:59 +0100

    SportsStore: application core: Add ProductController, List-View and default route

---
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using SportsStore.Domain.Abstract;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.GetProducts);
+        }
+    }
+}
