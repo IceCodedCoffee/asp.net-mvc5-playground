9c41e071228b44c81e0532ff4efd99894f4e0bb5
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-02-21 01:25:58 +0100

    SportsStore: application core: Setup domain model and repository pattern with mock implementation

--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,4 +1,7 @@
-﻿using Ninject;
+﻿using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
@@ -23,7 +26,16 @@ namespace SportsStore.WebUI.Infrastructure
         }
         private void AddBindings()
         {
-            // put bindings here
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+
+            mock.Setup(m => m.GetProducts).Returns(new List<Product> 
+            {
+                 new Product { Name = "Football", Price = 25 },
+                 new Product { Name = "Surf board", Price = 179 },
+                 new Product { Name = "Running shoes", Price = 95 }
+            });
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
