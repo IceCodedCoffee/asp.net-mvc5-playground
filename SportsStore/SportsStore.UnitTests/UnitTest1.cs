85461d3c9122c3d6dc1b63fe94606fe405c1cb12
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-02-23 22:07:29 +0100

    SportsStore: Pagination: Supply View/HtmlHelper with necessary data & unit test

--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -35,10 +35,10 @@ namespace SportsStore.UnitTests
             controller.PageSize = 3;
 
             // Act
-            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
 
             // Assert
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
 
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual(prodArray[0].Name, "P4");
@@ -73,5 +73,35 @@ namespace SportsStore.UnitTests
             result.ToString());
         }
 
+        [TestMethod]
+        public void Can_Send_Pagination_View_Model()
+        {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                 new Product {ProductID = 1, Name = "P1"},
+                 new Product {ProductID = 2, Name = "P2"},
+                 new Product {ProductID = 3, Name = "P3"},
+                 new Product {ProductID = 4, Name = "P4"},
+                 new Product {ProductID = 5, Name = "P5"}
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+
+            controller.PageSize = 3;
+
+            // Act
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+
+            // Assert
+            PagingInfo pageInfo = result.PagingInfo;
+
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
+        }
     }
 }
--- SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -17,10 +18,23 @@ namespace SportsStore.WebUI.Controllers
 
         public ViewResult List(int page = 1)
         {
-            return View(repository.Products
-                        .OrderBy(p => p.ProductID)
-                        .Skip((page - 1) * PageSize)
-                        .Take(PageSize));
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                                .OrderBy(p => p.ProductID)
+                                .Skip((page - 1) * PageSize)
+                                .Take(PageSize)
+                ,
+
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+
+            return View(model);
         }
     }
 }
