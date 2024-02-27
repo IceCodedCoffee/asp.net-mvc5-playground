eeaffb6c4063e834fd6acf431f6b036135b34b77
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-02-27 18:05:51 +0100

    SportsStore: Filter: Implement category filter & unit test

--- SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -16,11 +16,12 @@ namespace SportsStore.WebUI.Controllers
             this.repository = productRepository;
         }
 
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
             ProductsListViewModel model = new ProductsListViewModel
             {
                 Products = repository.Products
+                                .Where(p => category == null || p.Category == category)
                                 .OrderBy(p => p.ProductID)
                                 .Skip((page - 1) * PageSize)
                                 .Take(PageSize)
