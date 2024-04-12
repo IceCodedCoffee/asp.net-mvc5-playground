f679e23b495e1ec08ed91e1ec1947fe0fec9e91a
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-04-13 00:59:44 +0200

    SportsStore: Administration: Edit products & Unit tests

--- SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
 ﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace SportsStore.WebUI.Controllers
@@ -15,6 +17,31 @@ namespace SportsStore.WebUI.Controllers
         public ViewResult Index()
         {
             return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+
+                TempData["message"] = ($"{product.Name} has been saved");
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(product);
+            }
         }
     }
 }
