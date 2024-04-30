92c49e2d1952a13e14d609c3491d585962a3b1a6
wolfa <wolf.alexander.development@gmail.com>
2024-04-30 23:48:11 +0200

    [15] UrlsAndRoutes: Init & project setup

---
+++ [15] UrlsAndRoutes/UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Controller = "Home";
+            ViewBag.Action = "Index";
+
+            return View("ActionName");
+        }
+    }
+}
