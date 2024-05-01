9532fae091f947b0ac56fcea877124de90f8aa5d
wolfa <wolf.alexander.development@gmail.com>
2024-05-01 03:47:37 +0200

    [15][02] Done: Remap old URL to new controller/action

--- [15] UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ [15] UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -10,6 +10,7 @@ namespace UrlsAndRoutes
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             // 2.
+            routes.MapRoute("ShopIndex", "Shop/Index", new { controller = "Customer", action = "List" });
         }
     }
 }
