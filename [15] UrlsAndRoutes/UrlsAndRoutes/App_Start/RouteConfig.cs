9a4ce1d277bbdf2472467ea3588090fd58359b68
wolfa <wolf.alexander.development@gmail.com>
2024-05-01 14:17:53 +0200

    [15][06] Done: Add unit tests and test previous routes

--- [15] UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ [15] UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -9,8 +9,20 @@ namespace UrlsAndRoutes
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // 1.
+            routes.MapRoute("CustomerIndex", "Customer/Index", new { controller = "Customer", action = "Index" });
+
+            // 2.
+            routes.MapRoute("ShopIndex", "Shop/Index", new { controller = "Customer", action = "List" });
+
+            // 3.
+            routes.MapRoute("ControllerAction", "{controller}/{action}");
+
+            // 4.
+            routes.MapRoute("ControllerActionDefault", "{controller}/{action}", new { action = "List" });
+
             // 5.
-            routes.MapRoute("ControllerAction", "{controller}/{action}", new { controller = "Customer", action = "List"});
+            routes.MapRoute("ControllerDefaultActionDefault", "{controller}/{action}", new { controller = "Customer", action = "List"});
         }
     }
 }
