7b023ea7116c87dab91e66b28e6c069945178535
wolfa <wolf.alexander.development@gmail.com>
2024-05-17 08:11:18 +0200

    [16] AdvancedRouting: Init & project setup

---
+++ [16] AdvancedRouting/AdvancedRouting/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    [RoutePrefix("Users")]
+    public class CustomerController : Controller
+    {
+        [Route("~/Test")]
+        public ActionResult Index()
+        {
+            ViewBag.Controller = "Customer";
+            ViewBag.Action = "Index";
+
+            return View("ActionName");
+        }
+
+        [Route("Add/{user}/{id:int}")]
+        public string Create(string user, int id)
+        {
+            return $"User: {user}, ID: {id}";
+        }
+
+        [Route("Add/{user}/{password:alpha:length(6)}")]
+        public string ChangePass(string user, string password)
+        {
+            return $"ChangePass Method - User: {user}, Pass: {password}";
+        }
+
+        public ActionResult List()
+        {
+            ViewBag.Controller = "Customer";
+            ViewBag.Action = "List";
+
+            return View("ActionName");
+        }
+    }
+}
---
+++ [16] AdvancedRouting/AdvancedRouting/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace AdvancedRouting.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Controller = "Home";
+
+            ViewBag.Action = "Index";
+
+            return View("ActionName");
+        }
+
+        public ActionResult CustomVariables()
+        {
+            ViewBag.Controller = "Home";
+            ViewBag.Action = "CustomVariables";
+            ViewBag.CustomVariableId = RouteData.Values["id"];
+            ViewBag.CustomCatchallVariable = RouteData.Values["catchall"];
+
+            return View();
+        }
+    }
+}
